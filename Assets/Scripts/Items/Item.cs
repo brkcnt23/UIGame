838378322cd0754a26aa168343375eb27eac7b36
Muhammet Furkan AVCI <furkanavci25@gmail.com>
2024-11-25 03:11:30 +0300

    Text Fix

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -6,9 +6,9 @@ public class Item
     public int ID;         // Unique identifier for the item
     public string Name;    // Name of the item
     public int Value;      // Value of the item in silver
-    public ItemCategory Category; 
+    public ItemCategory Category;
 
-    public Item(int id, string name, int value,ItemCategory category)
+    public Item(int id, string name, int value, ItemCategory category)
     {
         ID = id;
         Name = name;
